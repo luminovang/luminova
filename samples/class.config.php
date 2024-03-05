46b2820959301ff33d477c395d4464cb1515e829
Chigozie Peter <ujahchigozie@gmail.com>
2024-03-05 15:13:01 -0800

    Add files via upload

---
+++ samples/class.config.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Luminova Framework
+ *
+ * @package Luminova
+ * @author Ujah Chigozie Peter
+ * @copyright (c) Nanoblock Technology Ltd
+ * @license See LICENSE file
+ */
+
+return [
+    'aliases' => [
+        //'FooAlias' => 'SomeLiterary\Foo',
+    ]
+];
