362a6a3e035ec860117abe154403652dae2a74c3
Chigozie Peter <ujahchigozie@gmail.com>
2024-03-06 00:34:57 +0800

    Update cli.php

--- routes/cli.php
+++ routes/cli.php
@@ -3,14 +3,13 @@
 /** @var \Luminova\Routing\Router $router */
 /** @var \App\Controllers\Application $app */
 
-
-/*
+/**
 * Register before middleware
 * A global middleware that run before and after each command is executed
-$router->authenticate(function(){
-    return Terminal::STATUS_OK;
-});
+*    $router->authenticate(function(){
+*        return Terminal::STATUS_OK;
+*    });
+*
+* @example $router->command("command", 'Command::run');
+* @example $router->command("test", 'Command::test');
 */
-
-$router->command("command", 'Command::run');
-$router->command("test", 'Command::test');
