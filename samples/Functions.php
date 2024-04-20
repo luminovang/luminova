36b9860d73826177a7af1f9536622dab2e60ae79
Peter BBT <ujahchigozie@gmail.com>
2024-04-21 04:41:17 +0800

    New update and changes

--- samples/Functions.php
+++ samples/Functions.php
@@ -10,8 +10,18 @@
 namespace App\Controllers\Utils;
 
 use \Luminova\Base\BaseFunction;
-
-class Func extends BaseFunction
+use \Luminova\Functions\IPAddress;
+use \Luminova\Functions\Files;
+use \Luminova\Functions\TorDetector;
+/**
+ * Functions methods.
+ *
+ * @method static TorDetector         tor()       @return TorDetector
+ * @method static Files               file()      @return Files
+ * @method static IPAddress           ip()        @return IPAddress
+ * @method static Maths               math()      @return Maths
+ * @method static array|string        escape($input, $context, $encoding)      @return array|string
+*/
+class Functions extends BaseFunction
 {
-
 }
