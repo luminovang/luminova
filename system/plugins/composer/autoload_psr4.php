1a56c26a0a4d40305b6dba7bc8195eb2173ea1d6
Peter BBT <ujahchigozie@gmail.com>
2024-05-22 08:52:40 +0800

    New update and changes

--- system/plugins/composer/autoload_psr4.php
+++ system/plugins/composer/autoload_psr4.php
@@ -6,23 +6,9 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname(dirname($vendorDir));
 
 return array(
-    'Twig\\' => array($vendorDir . '/twig/twig/src'),
-    'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
-    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
-    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
-    'Smarty\\' => array($vendorDir . '/smarty/smarty/src'),
     'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Psr\\Cache\\' => array($vendorDir . '/psr/cache/src'),
-    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
-    'PHPStan\\ExtensionInstaller\\' => array($vendorDir . '/phpstan/extension-installer/src'),
-    'Luminova\\Tests\\' => array($baseDir . '/tests'),
-    'Luminova\\Psr\\Cache\\' => array($vendorDir . '/nanoblocktech/psr-cache/src'),
     'Luminova\\' => array($baseDir . '/system', $vendorDir . '/luminovang/framework/src'),
-    'League\\MimeTypeDetection\\' => array($vendorDir . '/league/mime-type-detection/src'),
-    'League\\Flysystem\\Local\\' => array($vendorDir . '/league/flysystem-local'),
-    'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
-    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
-    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'App\\Controllers\\' => array($baseDir . '/app/Controllers'),
 );
--- system/plugins/composer/autoload_real.php
+++ system/plugins/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInitce7d6b0379bc9a35d3afd6dd2b0e131e
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInitce7d6b0379bc9a35d3afd6dd2b0e131e::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
