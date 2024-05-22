d945a84dffc1bf384b5f645a68e4f15d07749482
Peter BBT <ujahchigozie@gmail.com>
2024-05-22 08:56:38 +0800

    New update and changes

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,4 @@
 /** @var \Luminova\Routing\Router $router */
 /** @var \App\Controllers\Application $app */
 
-
-/**
- * Register Before Middleware
- * A global middleware that run before and after each request
- * 
- * @example $router->middleware('GET|POST', '/*', App\Controllers\YourMiddleware::class);
-*/
- 
-/**
- * Register router main page view
-*/
 $router->get('/', 'Welcome::info');
--- routes/cli.php
+++ routes/cli.php
@@ -1,19 +1,3 @@
 <?php 
-use \Luminova\Routing\Router;
 /** @var \Luminova\Routing\Router $router */
 /** @var \App\Controllers\Application $app */
-
-/**
-* Register before middleware
-* A global middleware that run before and after each command is executed
-* @example 
-*   $router->before(function(){
-*        return Terminal::STATUS_OK;
-*    });
-*/
-
-$router->group("blog", function(Router $router): void
-{
-    $router->command('list', 'BlogCommand::list');
-    $router->command('/read/id/(:value)', 'BlogCommand::read');
-});
