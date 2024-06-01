d390d95be9733e6210da4f200f88e697ab1fbe84
Peter BBT <ujahchigozie@gmail.com>
2024-06-01 19:24:25 +0800

    New update and changes

--- resources/views/system_errors/404.php
+++ resources/views/system_errors/404.php
@@ -5,24 +5,22 @@
     <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="./favicon.png">
-    <title>404 Error - <?= escape($name);?></title>
-    <style>
-        <?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
-    </style>
+    <title>404 View Error - <?= escape($title ?? '');?></title>
+    <style><?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')); ?></style>
 </head>
 <body id="e_all">
     <div class="container text-center main-container">
         <h1 class="headline">404</h1>
-        <p class="lead">Error Page</p>
+        <p class="lead">Error View Not Found</p>
     </div>
     <div class="footer">
         <div class="container">
 
             <p>
-                Displayed at <?= date('H:i:sA') ?> &mdash;
+                Displayed at <?= date('H:i:sA'); ?> &mdash;
                 PHP: <?= PHP_VERSION ?>  &mdash;
-                Luminova: <?= \Luminova\Application\Foundation::VERSION ?> &mdash;
-                Environment: <?= ENVIRONMENT ?>
+                Luminova: <?= \Luminova\Application\Foundation::VERSION; ?> &mdash;
+                Environment: <?= ENVIRONMENT; ?>
             </p>
 
         </div>
