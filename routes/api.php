6638a517c5c1b517ae8c80982b35c018040c949e
Luminova <dev@luminova.ng>
2025-12-12 04:53:15 +0800

    Application Method-based routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use \Luminova\Routing\Router;
 /**
  * This file handles all URI that start with `api` (e.g, https://example.com/api)
  * 
- * The following global variables are available in the current file:
+ * The following global variables are available in the current file: 
  * 
  * @var \Luminova\Routing\Router $router The routing object.
  * @var \Luminova\Foundation\Core\Application $app The application object.
